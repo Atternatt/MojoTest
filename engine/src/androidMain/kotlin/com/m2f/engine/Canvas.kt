7b09600796371f90be4cbf67f3c2efe81b2ee7a7
Marc Moreno Ferrer <ignis.fatue@gmail.com>
2022-06-08 18:56:27 +0200

    feat(mojo): Implement draw rect logic for multiplatform canvas
    At this moment we created a canvas in the engine module that is framework agnostic. It provides the logic to draw a rect as per our algebra at the
    moment is the only call that we need.

--- engine/src/androidMain/kotlin/com/m2f/engine/Canvas.kt
+++ engine/src/androidMain/kotlin/com/m2f/engine/Canvas.kt
@@ -1,10 +1,61 @@
 package com.m2f.engine
 
-import android.graphics.Canvas as ACanvas
+import android.util.Log
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.*
+import com.m2f.engine.model.*
+import com.m2f.engine.model.Color as EngineColor
 
-actual class Canvas(private val canvas: ACanvas) {
 
-    actual fun draw() {
+actual class Canvas(
+    scope: DrawScope,
+    bounds: Bounds = bounds(
+        position = position(
+            x = (scope.center.x - (scope.size.width / 2)) / scope.size.width,
+            y = (scope.center.y - (scope.size.height / 2)) / scope.size.height
+        ),
+        size = size(
+            width = 1f,
+            height = 1f
+        )
+    )
+) : Bounds by bounds, DrawScope by scope {
 
+    actual fun drawRect(color: EngineColor, padding: Padding, bounds: Bounds) {
+        val canvasW = this@Canvas.width * size.width
+        val canvasH = this@Canvas.height * size.height
+
+        val canvasX = (this@Canvas.x * size.width)
+        val canvasY = (this@Canvas.y * size.height)
+
+        val boundsWithPadding: Bounds = bounds.calculateBoundWithPadding(padding)
+        scale(1f, -1f) {
+
+            val w = boundsWithPadding.width * canvasW
+            val h = boundsWithPadding.height * canvasH
+            val x = boundsWithPadding.x * canvasW + canvasX
+            val y = boundsWithPadding.y * canvasH + canvasY
+
+            Log.d("Rel Bounds", "w= $w, h= $h, x= $x, y= $y, " +
+                    "canvasW= $canvasW " +
+                    "canvasH= $canvasH " +
+                    "canvasX= $canvasX " +
+                    "canvasY= $canvasY ")
+             drawRect(
+                color = Color(color.background),
+                size = Size(
+                    w,
+                    h
+                ),
+                topLeft = Offset(
+                    x,
+                    y
+                )
+            )
+        }
     }
+
+    actual fun copy(bounds: Bounds): Canvas = Canvas(this, bounds)
 }
--- engine/src/commonMain/kotlin/com/m2f/engine/Canvas.kt
+++ engine/src/commonMain/kotlin/com/m2f/engine/Canvas.kt
@@ -1,5 +1,10 @@
 package com.m2f.engine
 
-expect class Canvas {
-    fun draw()
+import com.m2f.engine.model.Bounds
+import com.m2f.engine.model.Color
+import com.m2f.engine.model.Padding
+
+expect class Canvas: Bounds {
+    fun drawRect(color: Color, padding: Padding, bounds: Bounds)
+    fun copy(bounds: Bounds): Canvas
 }
