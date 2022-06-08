e988582bf9612a33cbbc847fb8d2b3c5369d972d
Marc Moreno Ferrer <ignis.fatue@gmail.com>
2022-06-08 18:55:14 +0200

    feat(mojo): Update Item model structure to compose color, anchor and padding

--- engine/src/commonMain/kotlin/com/m2f/engine/model/Item.kt
+++ engine/src/commonMain/kotlin/com/m2f/engine/model/Item.kt
@@ -1,6 +1,9 @@
 package com.m2f.engine.model
 
-sealed interface Bounds : Position, Size
+import com.m2f.engine.Canvas
+
+//region Bounds
+interface Bounds : Position, Size
 
 internal data class BoundsData(private val position: Position, private val size: Size) : Bounds {
     override val x: Float = position.x
@@ -11,7 +14,9 @@ internal data class BoundsData(private val position: Position, private val size:
 }
 
 fun bounds(position: Position, size: Size): Bounds = BoundsData(position, size)
+//endregion
 
+//region Position
 sealed interface Position {
     val x: Float
     val y: Float
@@ -22,8 +27,10 @@ internal data class PositionData(
     override val y: Float
 ) : Position
 
-fun position(x: Float, y: Float): Position = PositionData(x, y)
+fun position(x: Float = 0f, y: Float = 0f): Position = PositionData(x, y)
+//endregion
 
+//region Size
 sealed interface Size {
     val width: Float
     val height: Float
@@ -32,18 +39,163 @@ sealed interface Size {
 internal data class SizeData(override val width: Float, override val height: Float) : Size
 
 fun size(width: Float, height: Float): Size = SizeData(width, height)
+//endregion
 
+//region Color
 sealed interface Color {
-    val background: String
+    val background: Long
 }
 
-internal data class ColorData(override val background: String) : Color
+internal data class ColorData(override val background: Long) : Color
 
-fun color(background: String): Color = ColorData(background)
+fun color(background: Long): Color = ColorData(background)
+//endregion
 
-class Item(
-    private val position: Position,
-    private val size: Size,
-    private val color: Color
+//region Padding
+sealed interface Padding {
+    val paddingTop: Float
+    val paddingBottom: Float
+    val paddingLeft: Float
+    val paddingRight: Float
+}
+
+internal data class FullPadding(val padding: Float) : Padding {
+    override val paddingTop: Float
+        get() = padding
+    override val paddingBottom: Float
+        get() = padding
+    override val paddingLeft: Float
+        get() = padding
+    override val paddingRight: Float
+        get() = padding
+
+}
+
+internal data class VerticalPadding(val padding: Float) : Padding {
+    override val paddingTop: Float
+        get() = padding
+    override val paddingBottom: Float
+        get() = padding
+    override val paddingLeft: Float
+        get() = 0f
+    override val paddingRight: Float
+        get() = 0f
+
+}
+
+internal data class HorizontalPadding(val padding: Float) : Padding {
+    override val paddingTop: Float
+        get() = 0f
+    override val paddingBottom: Float
+        get() = 0f
+    override val paddingLeft: Float
+        get() = padding
+    override val paddingRight: Float
+        get() = padding
+}
+
+internal data class TopPadding(val padding: Float) : Padding {
+    override val paddingTop: Float
+        get() = padding
+    override val paddingBottom: Float
+        get() = 0f
+    override val paddingLeft: Float
+        get() = 0f
+    override val paddingRight: Float
+        get() = 0f
+
+}
+
+internal data class BottomPadding(val padding: Float) : Padding {
+    override val paddingTop: Float
+        get() = 0f
+    override val paddingBottom: Float
+        get() = padding
+    override val paddingLeft: Float
+        get() = 0f
+    override val paddingRight: Float
+        get() = 0f
+
+}
+
+internal data class LeftPadding(val padding: Float) : Padding {
+    override val paddingTop: Float
+        get() = 0f
+    override val paddingBottom: Float
+        get() = 0f
+    override val paddingLeft: Float
+        get() = padding
+    override val paddingRight: Float
+        get() = 0f
+
+}
+
+internal data class RightPadding(val padding: Float) : Padding {
+    override val paddingTop: Float
+        get() = 0f
+    override val paddingBottom: Float
+        get() = 0f
+    override val paddingLeft: Float
+        get() = 0f
+    override val paddingRight: Float
+        get() = padding
+}
+
+internal data class ComposedPadding(
+    override val paddingTop: Float,
+    override val paddingBottom: Float,
+    override val paddingLeft: Float,
+    override val paddingRight: Float
+) : Padding
+
+fun noPadding(): Padding = fullPadding(0f)
+fun fullPadding(value: Float): Padding = FullPadding(value)
+fun verticalPadding(value: Float): Padding = VerticalPadding(value)
+fun horizontalPadding(value: Float): Padding = HorizontalPadding(value)
+fun topPadding(value: Float): Padding = TopPadding(value)
+fun bottomPadding(value: Float): Padding = BottomPadding(value)
+fun leftPadding(value: Float): Padding = LeftPadding(value)
+fun rightPadding(value: Float): Padding = RightPadding(value)
+fun padding(
+    paddingTop: Float,
+    paddingBottom: Float,
+    paddingLeft: Float,
+    paddingRight: Float
+): Padding = ComposedPadding(paddingTop, paddingBottom, paddingLeft, paddingRight)
+//endregion
+
+//region Anchor
+sealed interface Anchor
+
+sealed interface AnchorX : Anchor
+object Left : AnchorX
+object CenterX : AnchorX
+object Right : AnchorX
+
+sealed interface AnchorY : Anchor
+object Top : AnchorY
+object CenterY : AnchorY
+object Bottom : AnchorY
+//endregion
+
+data class Item internal constructor(
+    val position: Position,
+    val size: Size,
+    val color: Color,
+    val padding: Padding,
+    val anchorX: AnchorX,
+    val anchorY: AnchorY
 ) : Bounds by bounds(position, size),
-    Color by color
+    Color by color,
+    Padding by padding,
+    AnchorX by anchorX,
+    AnchorY by anchorY
+
+fun item(
+    size: Size,
+    color: Color,
+    position: Position = position(),
+    padding: Padding = noPadding(),
+    anchorX: AnchorX = Left,
+    anchorY: AnchorY = Bottom
+): Item = Item(position, size, color, padding, anchorX, anchorY)
